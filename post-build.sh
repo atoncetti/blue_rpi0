8f7f33ad8bc6a832b835172ac9e34dff8e8ff196
Antonio Toncetti <antonio.toncetti@gmail.com>
2021-12-28 00:02:50 +0100

    Added minimal bluetooth config.

--- post-build.sh
+++ post-build.sh
@@ -9,3 +9,6 @@ $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_D
 
 # Copy the fwup includes to the images dir
 cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
+
+# Create dbus socket directory
+mkdir -p $TARGET_DIR/run/dbus
